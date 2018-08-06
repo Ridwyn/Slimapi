b0847704b3633e1c88169c1c4299afb87a10a154
Ridwyn <salihouridwyn@gmail.com>
2018-08-06 02:06:23 +0100

    changes for customer.php

--- src/routes/customer.php
+++ src/routes/customer.php
@@ -7,6 +7,18 @@ $app =new \Slim\App;
 //to display error trace ad msg
 $app = new \Slim\App(['settings' => ['displayErrorDetails' => true]]);
 
+//Enable cross platform
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
+
 
 //Get All customers
 $app->get('/api/customers', function(Request $request, Response $response) {
